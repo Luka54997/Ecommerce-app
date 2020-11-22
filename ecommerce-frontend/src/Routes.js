04f5d7e3b6856192d97f356bb9381661a6b47abc
luka3213 <lukamilanovic689@gmail.com>
2020-11-22 21:21:26 +0100

    Added Shop Component

--- ecommerce-frontend/src/Routes.js
+++ ecommerce-frontend/src/Routes.js
@@ -9,6 +9,8 @@ import AdminDashboard from './user/AdminDashboard'
 import AdminRoute from './auth/AdminRoute'
 import AddCategory from './admin/AddCategory'
 import AddProduct from './admin/AddProduct'
+import Shop from './core/Shop'
+
 const Routes = () => {
 
     return(
@@ -18,6 +20,7 @@ const Routes = () => {
             <Route path ='/signin' exact component ={Signin}/>
             <Route path ='/signup' exact component ={Signup}/>
             <Route path ='/' exact component ={Home}/>
+            <Route path ='/shop' exact component ={Shop}/>
             <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
             <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
             <AdminRoute path="/create/category" exact component={AddCategory} />
