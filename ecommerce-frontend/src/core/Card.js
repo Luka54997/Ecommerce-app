9c191644b6e8656897cc399600bcf4a206f7c026
luka3213 <lukamilanovic689@gmail.com>
2020-12-13 23:03:20 +0100

    Added addItem to cart functionality

--- ecommerce-frontend/src/core/Card.js
+++ ecommerce-frontend/src/core/Card.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, Redirect} from 'react-router-dom'
 import ShowImage from './showImage'
 import moment from 'moment'
+import { addItem } from './cartHelpers'
 
 const Card = ({product, showViewProductButton = true}) =>{
+
+    const [redirect,setRedirect] = useState(false)
+
+    const addToCart = () =>{
+        addItem(product,()=>{
+            setRedirect(true)
+        })
+    }
+
+    const shouldRedirect = redirect =>{
+        if(redirect)
+            return <Redirect to='/cart'/>
+    }
 
 
     const showAddToCartButton = () =>{
         return (
-            <button className='btn btn-outline-warning mt-2 mb-2'>Add to cart</button>
+            <button onClick={addToCart} className='btn btn-outline-warning mt-2 mb-2'>Add to cart</button>
         )
     }
 
@@ -22,6 +36,7 @@ const Card = ({product, showViewProductButton = true}) =>{
             <div className='card'>
                 <div className='card-header name'>{product.name}</div>
                 <div className='card-body'>
+                    {shouldRedirect(redirect)}
                     <ShowImage item={product} url='product'></ShowImage>
                     <p className='lead'>{product.description}</p>
                     <p>${product.price}</p>
